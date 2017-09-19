64107fc6429502acf27038e4e35f4b957d25cfd6
KlaudiaPolan <polan.szlachta@gmail.com>
2017-09-19 23:23:03 +0200

    mouse moving element

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ bg.addEventListener('mousemove', (e) => {
 
   bg.style.transform = `translate3d(-${mouseX}%, -${mouseY}%, 0)`;
 });
+
+const macbook = document.querySelector('.macbook');
+const windowWidth2 = window.innerWidth / 10;
+const windowHeight2 = window.innerHeight / 10 ;
+
+macbook.addEventListener('mousemove', (e) => {
+  const mouseX = e.clientX / windowWidth2;
+  const mouseY = e.clientY / windowHeight2;
+
+  macbook.style.transform = `translate3d(-${mouseX}%, -${mouseY}%, 0)`;
+});
